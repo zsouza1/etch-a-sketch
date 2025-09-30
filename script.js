bc5af558bca5c89ec5eb1e4bb12f5b60bec64c06
zsouza1 <zsouza14@gmail.com>
2025-09-30 11:15:32 -0700

    Create functional grid with reset/prompt buttons

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
 const container = document.querySelector("#container");
+const gridContainer = document.querySelector("#grid-container");
+const buttonContainer = document.querySelector("#button-container");
+const STARTING_GRID = 10;
 
-
-const box = document.createElement("div");
-
-
-
-function createGrid() {
-    for (let i = 0; i < 100; i++) {
-        const box = document.createElement("div");
-        box.classList.add("box")
-        container.appendChild(box);
-        box.textContent = i
-        box.addEventListener("mouseenter", (event) => {
-        event.target.style.backgroundColor = "black";
-})
-    }
-}
-
-createGrid();
-
-const reset = document.createElement("button");
-container.appendChild(reset);
-reset.textContent = "Reset";
-reset.addEventListener("click", () => {
-    box.style.background = "grey";
+//grid creation function
+function createGrid(number) {
+    for (let i = 0; i < number; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
         
-   
-    console.log("click")
+        for (let i = 0; i < number; i++) {
+            const box = document.createElement("div")
+            box.classList.add("box");
+            box.addEventListener("mouseenter", (event) => {
+            event.target.style.backgroundColor = "black";
+            })
+            row.appendChild(box);
+        };
+
+        gridContainer.appendChild(row);
+    } 
+}
+createGrid(STARTING_GRID);
+
+
+const gridPrompt = document.createElement("button");
+gridPrompt.classList.add("prompt-button")
+buttonContainer.appendChild(gridPrompt);
+gridPrompt.textContent = "Grid Size";
+gridPrompt.addEventListener("click", () => {
+    let number = prompt("Grid Size?");
+    if (number > 100) {
+        number = 100;
+        alert("Grid size lowered to 100")
+    } else if ((number === null) || (number === "")) {
+        number = 10;
+    } ;
+    gridContainer.replaceChildren();
+    createGrid(number);
+    
+})
+
+
+
+
+
+
+
+const resetButton = document.createElement("button");
+buttonContainer.appendChild(resetButton);
+resetButton.textContent = "Reset color";
+resetButton.addEventListener("click", () => {
+    const resetGrid = document.querySelectorAll(".box")
+    resetGrid.forEach((box) => {
+        box.style.backgroundColor = "white"});
 });
-
-
 
